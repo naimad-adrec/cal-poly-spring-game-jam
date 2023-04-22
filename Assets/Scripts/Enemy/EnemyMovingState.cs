293baaa759d12765c6e25e5c99814b4250b60f32
naimad-adrec <106353663+naimad-adrec@users.noreply.github.com>
2023-04-22 11:58:08 -0700

    Enemy Input

--- Assets/Scripts/Enemy/EnemyMovingState.cs
+++ Assets/Scripts/Enemy/EnemyMovingState.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class EnemyMovingState : EnemyBaseState
 {
+    // Movement Variables
+    private float moveSpeed = 1f;
+
     public override void EnterState(EnemyStateMachine enemy)
     {
-
+        Debug.Log("Moving");
+        MoveToTarget(enemy);
     }
 
     public override void UpdateState(EnemyStateMachine enemy)
     {
+        if (enemy.AtTarget == false)
+        {
+            MoveToTarget(enemy);
 
+            // Attack damage player if collision detected
+        }
+        else
+        {
+            enemy.SwitchState(enemy.IdleState);
+        }
     }
 
-    public override void OnCollisionEnter2D(EnemyStateMachine enemy)
+    private void MoveToTarget(EnemyStateMachine enemy)
     {
+        enemy.transform.position = Vector2.MoveTowards(enemy.transform.position, enemy.TargetPosition.transform.position, moveSpeed * Time.deltaTime);
 
+        if (enemy.transform.position == enemy.TargetPosition.transform.position)
+        {
+            enemy.AtTarget = true;
+        }
     }
 }
