7b56853de50b29528b99ccdf6a3f28a71951bb37
naimad-adrec <106353663+naimad-adrec@users.noreply.github.com>
2023-04-22 18:27:42 -0700

    Adjusted spawn points

--- Assets/Scripts/TreeSpawnPoint.cs
+++ Assets/Scripts/TreeSpawnPoint.cs
@@ -1,13 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class TreeSpawnPoint : MonoBehaviour
 {
-    // Event Variables
-    [SerializeField] private UnityEvent spawnNewTrees;
-
     // Tree Variables
     [SerializeField] private GameObject tree;
     private float roundMax;
